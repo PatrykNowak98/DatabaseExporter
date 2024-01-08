b45c5f4fe096eefbbf6fdb957695f25d78b0279e
Patryk Nowak <patryk.nowak@taskscape.com>
2024-01-08 15:29:47 +0100

    Connection string via config

--- DatabaseExporter/Program.cs
+++ DatabaseExporter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.IO;
+using System.Configuration;
 
 namespace DatabaseExporter
 {
@@ -8,17 +9,14 @@ namespace DatabaseExporter
     {
         static void Main(string[] args)
         {
-            string connectionString = "Server=censored;Database=censored;User Id=censored;Password=censored;";
-            string outputDirectory = @"censored";
+            string connectionString = ConfigurationManager.ConnectionStrings["Initial"].ConnectionString;
+            string outputDirectory = ConfigurationManager.AppSettings["outputDirectory"];
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
 
                 ExportTables(connection, outputDirectory);
-                ExportViews(connection, outputDirectory);
-                ExportProcedures(connection, outputDirectory);
-                ExportFunctions(connection, outputDirectory);
 
                 Console.WriteLine("Export completed successfully.");
             }
@@ -38,63 +36,6 @@ namespace DatabaseExporter
                         string tableName = reader["TABLE_NAME"].ToString();
                         // Write the table name to the file
                         writer.WriteLine(tableName);
-                    }
-                }
-            }
-        }
-
-        static void ExportViews(SqlConnection connection, string outputDirectory)
-        {
-            // Fetch views
-            using (SqlCommand command = new SqlCommand("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.VIEWS", connection))
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                // Create or overwrite the views.txt file
-                using (StreamWriter writer = new StreamWriter(Path.Combine(outputDirectory, "views.txt")))
-                {
-                    while (reader.Read())
-                    {
-                        string viewName = reader["TABLE_NAME"].ToString();
-                        // Write the view name to the file
-                        writer.WriteLine(viewName);
-                    }
-                }
-            }
-        }
-
-        static void ExportProcedures(SqlConnection connection, string outputDirectory)
-        {
-            // Fetch procedures
-            using (SqlCommand command = new SqlCommand("SELECT ROUTINE_NAME FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = 'PROCEDURE'", connection))
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                // Create or overwrite the procedures.txt file
-                using (StreamWriter writer = new StreamWriter(Path.Combine(outputDirectory, "procedures.txt")))
-                {
-                    while (reader.Read())
-                    {
-                        string procedureName = reader["ROUTINE_NAME"].ToString();
-                        // Write the procedure name to the file
-                        writer.WriteLine(procedureName);
-                    }
-                }
-            }
-        }
-
-        static void ExportFunctions(SqlConnection connection, string outputDirectory)
-        {
-            // Fetch functions
-            using (SqlCommand command = new SqlCommand("SELECT ROUTINE_NAME FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = 'FUNCTION'", connection))
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                // Create or overwrite the functions.txt file
-                using (StreamWriter writer = new StreamWriter(Path.Combine(outputDirectory, "functions.txt")))
-                {
-                    while (reader.Read())
-                    {
-                        string functionName = reader["ROUTINE_NAME"].ToString();
-                        // Write the function name to the file
-                        writer.WriteLine(functionName);
                     }
                 }
             }
